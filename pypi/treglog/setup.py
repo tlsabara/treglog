091793b890495608208da420c32dac22a533e8ff
Thiago Sabará <78706759+tlsabara@users.noreply.github.com>
2022-03-13 13:56:04 -0300

    Documentations

---
+++ pypi/treglog/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'aluratemp'
+    version = '2.0.0',
+    author = 'Thiago Sabará',
+    author_email = 'thi.sil.sab@gmail.com',
+    packages = ['treglog']
+    description = 'Um simples projeto de logs, para pequenos projetos',
+    long_description = 'Um simples projeto de logs, para pequenos projetos. '
+                        + 'Consegue criar modos de debug para log em arquivo e em tela.'
+    url = 'https://github.com/tlsabara/treglog/',
+    project_urls = {
+        'Código fonte': 'https://github.com/yanorestes/aluratemp',
+        'Download': 'https://github.com/yanorestes/aluratemp/archive/1.0.0.zip'
+    }
+)
